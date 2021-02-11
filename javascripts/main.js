a75faa5765fa651d341973cd6a6fd19f3b6e1af8
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2021-02-11 22:08:55 +0900

    テトリスが落ちる動きを追加

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,9 +21,7 @@ var array = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-function 
-
-draw() {
+function draw() {
   $('#game').find('tr').each(function(i, elemTr) { // trタグそれぞれに対する処理
       $(elemTr).children().each(function(j, elemTd) { // tdタグそれぞれに対する処理
           $(elemTd).removeClass(); // まずはクラスをすべてなしにする
@@ -38,7 +36,35 @@ draw() {
   });
 }
 
+function fall() {
+  var under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]; // 下の行にブロックがあるかどうかを保持する配列
+  for (var i = 19; i >= 0; i--) {
+      for (var j = 0; j < 10; j++) {
+          if (under[j] == 0) {
+              if (array[i][j] == 0) {
+                  // 下に何もなくそのマスがブロックでもないとき
+                  under[j] = 0;
+              } else {
+                  // 下に何もなくそのマスがブロックであるとき
+                  array[i + 1][j] = array[i][j];
+                  array[i][j] = 0;
+                  under[j] = 0;
+              }
+          } else {
+              if (array[i][j] == 0) {
+                  // 下がブロックでそのマスがブロックでないとき
+                  under[j] = 0;
+              } else {
+                  // 下がブロックでそのマスがブロックのとき
+                  under[j] = 1;
+              }
+          }
+      }
+  }
+}
+
 draw(); // 読込が完了したらまず表示
 setInterval(function() {
+  fall();
   draw();
 }, 500); // 0.5秒ごとに表示を更新していきます
